6fbf206aa9aee281c16f8f251afc3703c52f9df0
Perkles <lucasbatistasuzart@outlook.com>
2022-05-21 00:27:47 -0300

    Adicionar arquivos de projeto.

---
+++ testeWform/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿using System;
+
+namespace testeWform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.Salvar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstContatos = new System.Windows.Forms.ListView();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_Sair = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(84, 84);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(84, 38);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(84, 137);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // Salvar
+            // 
+            this.Salvar.Location = new System.Drawing.Point(109, 178);
+            this.Salvar.Name = "Salvar";
+            this.Salvar.Size = new System.Drawing.Size(75, 23);
+            this.Salvar.TabIndex = 4;
+            this.Salvar.Text = "Salvar";
+            this.Salvar.UseVisualStyleBackColor = true;
+            this.Salvar.Click += new System.EventHandler(this.Salvar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "NOME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "TELEFONE";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "EMAIL";
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(664, 23);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 6;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Buscar";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lstContatos
+            // 
+            this.lstContatos.HideSelection = false;
+            this.lstContatos.Location = new System.Drawing.Point(265, 57);
+            this.lstContatos.Name = "lstContatos";
+            this.lstContatos.Size = new System.Drawing.Size(474, 144);
+            this.lstContatos.TabIndex = 9;
+            this.lstContatos.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(265, 25);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(375, 20);
+            this.txtBuscar.TabIndex = 5;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(810, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // btn_Sair
+            // 
+            this.btn_Sair.Location = new System.Drawing.Point(664, 229);
+            this.btn_Sair.Name = "btn_Sair";
+            this.btn_Sair.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sair.TabIndex = 12;
+            this.btn_Sair.Text = "Sair";
+            this.btn_Sair.UseVisualStyleBackColor = true;
+            this.btn_Sair.Click += new System.EventHandler(this.btn_Sair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 292);
+            this.Controls.Add(this.btn_Sair);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.lstContatos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Salvar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button Salvar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView lstContatos;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Button btn_Sair;
+    }
+}
+
---
+++ testeWform/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using MySql.Data.MySqlClient;
+
+namespace testeWform
+
+{
+    public partial class Form1 : Form   
+    {
+       private SqlConnection Conexao;
+        private string data_source = "server=MEGAWARE-W240HU;database=db_agenda;user id=sa;pwd=123456";
+        public Form1()
+        {
+            InitializeComponent();
+            txtNome.Focus();
+            txtTelefone.Focus();
+
+            lstContatos.View = View.Details;
+            lstContatos.LabelEdit = true;
+            lstContatos.AllowColumnReorder = true;
+            lstContatos.FullRowSelect = true;
+            lstContatos.GridLines = true;
+           //stContatos.Columns.Add("ID",140, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("Nome", 140, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("E-mail", 140, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("Telefone", 30, HorizontalAlignment.Left);
+
+        }
+
+        private void Salvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //criar conexao com o MySql
+
+                // executar o comando Insert 
+
+                // string data_source = "data_source=localhost;username=root;password=;database=db_agenda";
+                SqlCommand cmd = new SqlCommand();
+                Conexao = new SqlConnection(data_source);
+                Conexao.Open();
+
+                
+                txtNome.Text = txtNome.Text.ToUpper();
+                txtEmail.Text = txtEmail.Text.ToUpper();
+               // string sql = "INSERT INTO contatos (nome,email,telefone)" + "VALUES" + "('" + txtNome.Text + "','" + txtEmail.Text + "','" + txtTelefone.Text + "')";
+
+               // SqlCommand cmd = new SqlCommand();
+                cmd.Connection = Conexao;
+
+
+                cmd.CommandText = "INSERT INTO contatos (nome,email,telefone)" + "VALUES" +
+                                   "(@nome,@email,@telefone)";
+
+
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+
+                cmd.ExecuteNonQuery();
+
+
+
+                //cmd.CommandText = "INSERT INTO contatos (nome,email,telefone)" + "VALUES" +
+                // "('" + txtNome.Text + "','" + txtEmail.Text + "','" + txtTelefone.Text + "')";
+
+               // Conexao.Open();
+                //comando.ExecuteReader();
+              cmd.ExecuteReader();
+               // MessageBox.Show("deu certo");
+
+
+            }
+            catch (SqlException ex) {
+
+                MessageBox.Show("Erro " + ex.Number + " ocorreu: " + ex.Message,
+        "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+                }
+
+
+
+
+            finally
+            {
+                Conexao.Close();
+            }
+
+            
+            }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                                
+                // string data_source = "data_source=localhost;username=root;password=;database=db_agenda";
+                Conexao = new SqlConnection(data_source);
+
+                //string sql = "INSERT INTO contatos (nome,email,telefone)" + "VALUES" + "('" + txtNome.Text + "','" + txtEmail.Text + "','" + txtTelefone.Text + "')";
+                string q = "'%" + txtBuscar.Text + "%'";
+
+                string buscar = "select * " +
+                                "From contatos" +
+                                " WHERE nome LIKE " + q + "OR email LIKE " + q;
+
+                Conexao.Open();
+                SqlCommand comando = new SqlCommand(buscar, Conexao);
+
+                
+                SqlDataReader reader = comando.ExecuteReader();
+
+                lstContatos.Items.Clear();
+
+                while (reader.Read())
+
+                {
+                    string[] row =
+                    {
+                        //Convert.ToString.reader.GetString(0),
+               reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                };
+                  var linha_listview = new ListViewItem(row);
+                    lstContatos.Items.Add(linha_listview);
+                    //MessageBox.Show(buscar);
+
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            
+
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Sair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }   
+
+            }
+
+
+        
+
+        
+
+       
